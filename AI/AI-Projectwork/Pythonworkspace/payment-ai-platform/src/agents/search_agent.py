80cd94650c716a75fbb97e3fedb297ea2a76d2ba
ramkotni <ramkotni@gmail.com>
2026-06-27 12:56:52 -0500

    checked in latest

--- AI/AI-Projectwork/Pythonworkspace/payment-ai-platform/src/agents/search_agent.py
+++ AI/AI-Projectwork/Pythonworkspace/payment-ai-platform/src/agents/search_agent.py
@@ -2,9 +2,9 @@ from rag.retriever import retriever
 
 def search_agent(state):
 
-    query = state["question"]
-
-    docs = retriever.invoke(query)
+    docs = retriever.invoke(
+        state["question"]
+    )
 
     context = "\n".join(
         [doc.page_content for doc in docs]
